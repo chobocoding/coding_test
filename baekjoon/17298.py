3a8e8944a904e13f37f5781dd896318e4d5a2e14
chobocoding <81543714+chobocoding@users.noreply.github.com>
2022-04-06 10:30:40 +0900

    자료구조 (☆☆)

---
+++ baekjoon/17298.py
@@ -0,0 +1,13 @@
+# https://www.acmicpc.net/problem/17298 
+
+import sys
+N = int(sys.stdin.readline())
+A = list(map(int, sys.stdin.readline().split()))
+NGE = [-1] * N
+index = []
+for i in range(0, N):
+    while len(index) != 0 and A[index[-1]] < A[i]:
+        NGE[index.pop()] = A[i]
+    index.append(i)
+for n in range(len(NGE)):
+    print(NGE[n], end=' ')
