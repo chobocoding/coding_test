8bc53a14a2f3c6bd61c7a464398767070cd0a3a6
chobocoding <81543714+chobocoding@users.noreply.github.com>
2022-04-06 10:33:40 +0900

    자료구조 (★★★)

--- baekjoon/17298.py
+++ baekjoon/17298.py
@@ -11,3 +11,8 @@ for i in range(0, N):
     index.append(i)
 for n in range(len(NGE)):
     print(NGE[n], end=' ')
+
+'''
+점점 큰 수를 찾으면서 왼쪽 수와 비교하면서 NGE값을 업데이트 해나감..
+'''
+# 코드 출처 : https://blog.naver.com/dhkimxx/222657694311
